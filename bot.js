6ce7964fd44fa0e691fc31c2cd11615c0a80d318
rafiksolo <63408024+rafiksolo@users.noreply.github.com>
2020-04-09 15:08:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const myid = ['697793948952952852']
-const client = new Discord.Client();"Njk3NzkzOTQ4OTUyOTUyODUy.Xo8g0w.kDRjUlcKLjL_vpMakeEkV5bCrfY"
+const client = new Discord.Client("Njk3NzkzOTQ4OTUyOTUyODUy.Xo8g0w.kDRjUlcKLjL_vpMakeEkV5bCrfY");
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
@@ -27,7 +27,7 @@ const client24 = new Discord.Client();
 
 
 
-client.login(process.env.TOKEN);"Njk3NzkzOTQ4OTUyOTUyODUy.Xo8g0w.kDRjUlcKLjL_vpMakeEkV5bCrfY"
+client.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
 client3.login(process.env.TOKEN3);
 client4.login(process.env.TOKEN4);
